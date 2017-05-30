83077ef950b6cca23959eada8b2ab7ec5f2d4d64
Grant <gsbackes@gmail.com>
2017-05-30 17:00:07 +0900

    Assignment 9: add callbacks and refactor code

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,17 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
 
-	var revealPoint = function(i) {
-		points[i].style.opacity = 1;
-		points[i].style.transform = "scaleX(1) translateY(0)";
-		points[i].style.msTransform = "scaleX(1) translateY(0)";
-		points[i].style.webkitTransform = "scaleX(1) translateY(0)";
+	var revealPoint = function(index) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.webkitTransform = "scaleX(1) translateY(0)";
 	};
 
-	for (var i = 0; i < points.length; i++) {
-		revealPoint(i);
-	}
+	forEach(points, revealPoint);
+	// for (var i = 0; i < points.length; i++) {
+	// 	revealPoint(i);
+	// }
 };
 
 window.onload = function() {
---
+++ scripts/utilities.js
@@ -0,0 +1,7 @@
+function forEach(points, callback) {
+	for (var i = 0; i <= points.length; i++) {
+		callback(i);
+	}
+}
+
+
