0b4a930bacb232248c0c2bc18e51f1e3bc49fc50
Grant <gsbackes@gmail.com>
2017-06-18 22:26:56 +0900

    Checkpoint 16: change landing page JS to jQuery.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,61 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-	var revealPoint = function(index) {
-		points[index].style.opacity = 1;
-		points[index].style.transform = "scaleX(1) translateY(0)";
-		points[index].style.msTransform = "scaleX(1) translateY(0)";
-		points[index].style.webkitTransform = "scaleX(1) translateY(0)";
+// ============= USE JQUERY ================
+var animatePoints = function () {
+	var revealPoint = function() {
+		$(this).css({
+			opacity: 	1, 
+			transform: 	"scaleX(1) translateY(0)"
+		});
 	};
 
-	forEach(points, revealPoint);
-	// for (var i = 0; i < points.length; i++) {
-	// 	revealPoint(i);
-	// }
+	$.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-	if (window.innerHeight > 950) {
-		animatePoints(pointsArray);
+$(window).load(function() {
+	if( $(window).height > 950 ) {
+		animatePoints();
 	}
-	var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top
-						- window.innerHeight + 200;
-	window.addEventListener('scroll', function(e){
-		if(document.documentElement.scrollTop || document.body.scrollTop
-			>= scrollDistance) {
-			animatePoints(pointsArray);
+
+	// Distance you must scroll before script triggered
+	var scrollDistance = $('.selling-points').offset().top 
+						- $(window).height() + 200;
+
+	$(window).scroll(function(event) {
+		if( $(window).scrollTop() >= scrollDistance) {
+			animatePoints();
 		}
 	});
-}
+});
+
+//============ Vanila JavaScript =============
+// var pointsArray = document.getElementsByClassName('point');
+
+// var animatePoints = function(points) {
+
+// 	var revealPoint = function(index) {
+// 		points[index].style.opacity = 1;
+// 		points[index].style.transform = "scaleX(1) translateY(0)";
+// 		points[index].style.msTransform = "scaleX(1) translateY(0)";
+// 		points[index].style.webkitTransform = "scaleX(1) translateY(0)";
+// 	};
+
+// 	forEach(points, revealPoint);
+// 	// for (var i = 0; i < points.length; i++) {
+// 	// 	revealPoint(i);
+// 	// }
+// };
+
+// window.onload = function() {
+// 	if (window.innerHeight > 950) {
+// 		animatePoints(pointsArray);
+// 	}
+// 	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+// 	var scrollDistance = sellingPoints.getBoundingClientRect().top
+// 						- window.innerHeight + 200;
+// 	window.addEventListener('scroll', function(e){
+// 		if(document.documentElement.scrollTop || document.body.scrollTop
+// 			>= scrollDistance) {
+// 			animatePoints(pointsArray);
+// 		}
+// 	});
+// }
 
